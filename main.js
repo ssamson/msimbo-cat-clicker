834a1593673af35594075a39e233c1ff5b2da62c
Stephanie Samson <sasamson98@hotmail.com>
2019-11-05 09:24:58 -0500

    added second cat

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 // Select the number of clicks
-const clicks = document.querySelector('#click');
+const cat1Clicks = document.querySelector('#cat1Click');
+const cat2Clicks = document.querySelector('#cat2Click');
 // Init number of clicks
-clicks.textContent = 0;
+// clicks.textContent = 0;
+cat1Click.textContent = 0;
+cat2Click.textContent = 0;
 
 // function to handle clicks on cat image
-function catClickHandler() {
-    clicks.textContent++;
-}
+// function catClickHandler() {
+//     clicks.textContent++;
+// }
 
 // listen to the clicks on cat image
-const cat = document.querySelector('img');
-cat.addEventListener('click', catClickHandler);
+const cat1 = document.querySelector('#cat1');
+const cat2 = document.querySelector('#cat2');
+
+// cat.addEventListener('click', catClickHandler);
+
+cat1.addEventListener('click', () => {
+    cat1Clicks.textContent++;
+});
+
+cat2.addEventListener('click', () => {
+    cat2Clicks.textContent++;
+});
